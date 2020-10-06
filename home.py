bf3d05d73c46aebc85ea69ee87f0edae6848409a
Nastasia Klevak <nastasiak.1214@gmail.com>
2020-10-06 14:34:17 -0400

    bootstrap again

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder='./ecoverde-master')
 
 @app.route('/')
 def show_home():
