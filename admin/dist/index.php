efaa18396d76cad5821fbca4e8a5c4809090890c
marinabeshai <48925086+marinabeshai@users.noreply.github.com>
2020-10-11 22:09:37 -0400

    Update index.php

--- admin/dist/index.php
+++ admin/dist/index.php
@@ -16,7 +16,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
     </head> -->
     
- <?php include ("/header.html"); ?>
+ <?php include ("/header.php"); ?>
 
     <body class="sb-nav-fixed">
      
