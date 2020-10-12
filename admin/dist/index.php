5c31ac6959581d194d021b35555d5443d141ba73
marinabeshai <48925086+marinabeshai@users.noreply.github.com>
2020-10-11 22:41:11 -0400

    Update index.php

--- admin/dist/index.php
+++ admin/dist/index.php
@@ -26,7 +26,7 @@
 <?php ini_set( 'error_reporting', E_ALL );
 ini_set( 'display_errors', true );
 
-      require ('admin/dist/header.php'); ?>
+      require ('../dist/header.php'); ?>
 
 
     </head>
