31f149e05c442336ac6a65ad11d2e447bf321805
marinabeshai <48925086+marinabeshai@users.noreply.github.com>
2020-10-11 22:10:39 -0400

    Update header.php

--- admin/dist/header.php
+++ admin/dist/header.php
@@ -1,4 +1,4 @@
-        <head>
+      <!--  <head> --> 
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -10,5 +10,4 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900&display=swap" rel="stylesheet">
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
-        </head> 
-        
+       <!-- </head> -->
