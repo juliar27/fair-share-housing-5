702c3ebd9b5ba7d3a81c5df2112c033ad0679a3b
marinabeshai <48925086+marinabeshai@users.noreply.github.com>
2020-10-11 22:53:37 -0400

    Update index.php

--- admin/dist/index.php
+++ admin/dist/index.php
@@ -17,6 +17,8 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
 -->
 
+       <?php  include "header.php"; ?>  
+
       </head>  
  
  
