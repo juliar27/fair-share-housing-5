07dbaa582e454ea16abfa19d439aebd6a93972d9
Julia Ruskin <jruskin@princeton.edu>
2020-10-17 17:30:10 -0400

    fix admin tables

--- data/database.py
+++ data/database.py
@@ -138,5 +138,46 @@ class Database:
         return rows
     # ------------------------------------------------------------------------------------------------------------------
 
+    def get_row(self, listingid):
+        cursor = self._connection.cursor()
+        stmt = "SELECT listings.*, cities.municipality, counties.county, counties.region FROM " + \
+                "listings, cities, counties WHERE listings.listingid = " + str(listingid) + " AND " + \
+                "listings.municode = cities.municode AND cities.county = counties.county"
+        cursor.execute(stmt)
+        row = cursor.fetchone()
+        result = {}
+        result['name'] = row[1]
+        result['developer'] = row[2]
+        result['status'] = row[3]
+        result['compliance'] = row[4]
+        result['vli1'] = row[6]
+        result['vli2'] = row[7]
+        result['vli3'] = row[8]
+        result['li1'] = row[9]
+        result['li2'] = row[10]
+        result['li3'] = row[11]
+        result['m1'] = row[12]
+        result['m2'] = row[13] 
+        result['m3'] = row[14]
+        result['vssn'] = row[15]
+        result['lssn'] = row[16]
+        result['mssn'] = row[17]
+        result['famsale'] = row[18]
+        result['famrent'] = row[19]
+        result['srsale'] = row[20]
+        result['srrent'] = row[21]
+        result['ssnsale'] = row[22]
+        result['ssnrent'] = row[23]
+        result['total'] = row[24]
+        result['family'] = row[25]
+        result['senior'] = row[26]
+        result['ssn'] = row[27]
+        result['total1'] = row[28]
+        result['total2'] = row[29]
+        result['total3'] = row[30]
+        result['muni'] = row[31]
+        result['county'] = row[32]
+        cursor.close()
+        return result
 # ----------------------------------------------------------------------------------------------------------------------
 
