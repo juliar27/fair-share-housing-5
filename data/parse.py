15d629493a5eceb31aebe02c30a0927539d0ff6b
Marina Beshai <mbeshai@princeton.edu>
2020-10-19 22:16:55 -0400

    Error catching uniqueid

--- data/parse.py
+++ data/parse.py
@@ -63,8 +63,18 @@ def get_listings(sheet, database):
             continue
         record = {}
 
-        if row[d['UNIQUEID']].ctype == XL_CELL_NUMBER:
-            record['listingid'] = str(int(row[d['UNIQUEID']].value))
+        try:
+            if row[d['UNIQUEID']].ctype == XL_CELL_NUMBER:
+                record['listingid'] = str(int(row[d['UNIQUEID']].value))
+            elif row[d['UNIQUEID']].value.strip() == "":
+                pass
+            else:
+                missing_columns, err = error('UNIQUEID', row_number, missing_columns)
+                errors.append(err)
+        except:
+            missing_columns, err = error('UNIQUEID', row_number, missing_columns, missing_column=True)
+            if err != "":
+                missing_columns_errors.append(err)
 
         try:
             if row[d['Municode']].ctype == XL_CELL_NUMBER:
