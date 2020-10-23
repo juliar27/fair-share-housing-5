f7859e4d897f82fa2974e80f12d73cf5c0dcce14
Julia Ruskin <jruskin@princeton.edu>
2020-10-23 01:34:12 -0400

    fixed header scroll

--- static/assets/demo/datatables-demo.js
+++ static/assets/demo/datatables-demo.js
@@ -2,6 +2,8 @@
 $(document).ready(function() {
   $('#dataTable').DataTable({
     "scrollY": "500px",
-    "paging": false
+    "paging": false,
+    "sScrollX":"100%",
+    "scrollCollapse": true
   });
 });
