675332ecbad5850bfb467b5158512ec6c7b4ea00
Marina Beshai <mbeshai@princeton.edu>
2020-11-03 12:49:32 -0500

    login redirect wasnt working

--- py/account.py
+++ py/account.py
@@ -179,6 +179,13 @@ def recovery(dict):
         if verified:
             id = id[0]
 
+            query = "SELECT temp_id from users where email = %s ;;"
+            cursor.execute(query, [email])
+
+            dup = cursor.fetchone()
+            print(dup)
+
+
             i = 0
             while i < 10:
                 id += random.choice(string.ascii_letters)
