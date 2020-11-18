349de574565298cdde1db9d62a6e9bc587945ccc
Marina Beshai <mbeshai@princeton.edu>
2020-11-18 12:32:31 -0500

    Working on emails

--- py/auth.py
+++ py/auth.py
@@ -2,19 +2,47 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from jinja2 import Environment
+from threading import Thread
+
+class Server:
+    def __init__(self):
+        self._context = ssl.create_default_context()
+        self._server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=self._context)
+
+    def sendEmail(self, subject, receiver_email, html, link):
+        sender_email = "nobodyatmapfsh@gmail.com"
+        password = "welovebob"
+
+        self._server.login(sender_email, password)
+        message = MIMEMultipart("alternative")
+        message["Subject"] = subject
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        part2 = MIMEText(
+            Environment().from_string(html).render(
+                link=link
+            ), "html"
+        )
+
+        message.attach(part2)
+
+        self._server.sendmail(sender_email, receiver_email, message.as_string())
+
+class EmailThreads (Thread):
+    def __init__(self, server, receiver_email, html, subject, link):
+        Thread.__init__(self)
+        self._server = server
+        self._receiver_email = receiver_email
+        self._html = html
+        self._subject =  subject
+        self._link = link
+
+    def run(self):
+        self._server.sendEmail(self._subject, self._receiver_email, self._html, self._link)
 
 
 def auth_email(receiver_email, link):
-    sender_email = "nobodyatmapfsh@gmail.com"
-    password = "welovebob"
-
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Verify your mapFSH Account"
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Create the plain-text and HTML version of your message
-
     html = """\
         <html>
           <body>
@@ -27,37 +55,13 @@ def auth_email(receiver_email, link):
         </html>
         """
 
-    # Turn these into plain/html MIMEText objects
-    # part2 = MIMEText(html, "html")
+    server = Server()
+    thread = EmailThreads(server, receiver_email, html, "Verify your mapFSH Account", link)
+    thread.start()
 
-    part2 = MIMEText(
-        Environment().from_string(html).render(
-            link=link
-        ), "html"
-    )
-
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part2)
-
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
 
 
 def recovery_email(receiver_email, link):
-    sender_email = "nobodyatmapfsh@gmail.com"
-    password = "welovebob"
-
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "Password Recovery"
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Create the plain-text and HTML version of your message
-
     html = """\
         <html>
           <body>
@@ -73,21 +77,6 @@ def recovery_email(receiver_email, link):
         </html>
         """
 
-    # Turn these into plain/html MIMEText objects
-    # part2 = MIMEText(html, "html")
-
-    part2 = MIMEText(
-        Environment().from_string(html).render(
-            link=link
-        ), "html"
-    )
-
-    # Add HTML/plain-text parts to MIMEMultipart message
-    # The email client will try to render the last part first
-    message.attach(part2)
-
-    # Create secure connection with server and send email
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+    server = Server()
+    thread = EmailThreads(server, receiver_email, html, "Reset your mapFSH Password", link)
+    thread.start()
